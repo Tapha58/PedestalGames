33ec2904e8d337ecc1620d059c06f8ae341bf934
Evgeniy <lucksergeev@gmail.com>
2020-05-15 17:42:46 +0300

    15052020

--- src/components/AdvancedSettings.vue
+++ src/components/AdvancedSettings.vue
@@ -117,6 +117,7 @@
             input: '',
             search: '',
             id_group: '',
+            x: {},
             advanced_settings_textareas: [
                 {
                     name: 'Игрок выиграл',
@@ -370,13 +371,16 @@
                 // this.advanced_settings_textareas[index].value = this.gameData.enable_notifications_desc
             },
             start_game: function () {
-                bridge.send("VKWebAppGetAuthToken", {"app_id": 7413476, "scope": "photos"});
+               this.x =  bridge.send("VKWebAppGetAuthToken", {"app_id": 7413476, "scope": "photos"});
+
+
+
                 // bridge.send("VKWebAppShowWallPostBox", {
                 //     "owner_id": -168555251,
                 //     "message": 'hello group',
                 //     "from_group": "1"
                 // });
-                console.log('sent')
+                console.log('sent' + this.x )
             },
         },
         components: {
